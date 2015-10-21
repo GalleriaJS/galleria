e5114e89cbd7529087d01d091ea3dec7d3028b38
Brandon Winch <brandonwinch@Brandons-MacBook-Pro.local>
2015-10-21 14:12:38 +1000

    slowing down settimeout for fast click

--- src/galleria.js
+++ src/galleria.js
@@ -1088,7 +1088,7 @@ $.event.special['click:fast'] = {
         }).on('touchstart.fast', function(e) {
             window.clearTimeout($(this).data('timer'));
             $(this).data('clickstate', {
-                touched: true, 
+                touched: true,
                 touchdown: true,
                 coords: getCoords(e.originalEvent),
                 evObj: e
@@ -1096,9 +1096,9 @@ $.event.special['click:fast'] = {
         }).on('touchmove.fast', function(e) {
             var coords = getCoords(e.originalEvent),
                 state = $(this).data('clickstate'),
-                distance = Math.max( 
-                    Math.abs(state.coords.x - coords.x), 
-                    Math.abs(state.coords.y - coords.y) 
+                distance = Math.max(
+                    Math.abs(state.coords.x - coords.x),
+                    Math.abs(state.coords.y - coords.y)
                 );
             if ( distance > 6 ) {
                 $(this).data('clickstate', $.extend(state, {
@@ -1113,7 +1113,7 @@ $.event.special['click:fast'] = {
             }
             $this.data('timer', window.setTimeout(function() {
                 $this.data('clickstate', def);
-            }, 400));
+            }, 500));
         }).on('click.fast', function(e) {
             var state = $(this).data('clickstate');
             if ( state.touched ) {
@@ -2758,7 +2758,7 @@ Galleria.prototype = {
 
             // legacy patch
             if( s === false || s == 'disabled' ) { return false; }
-            
+
             return !!Galleria.TOUCH;
 
         }( options.swipe ));
